d9f26aafa004cabb203a59a02548a1791f52a0bb
Samantha-Bitbucket <ottavianisamantha@gmail.com>
2021-03-17 09:38:10 +0100

    rutas users OK + probando  getAll() function

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/usuarios', usuariosRouter);
+app.use('/users', usuariosRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,9 +5,47 @@ const { getAll } = require('../models/usuario');
 
 /* Probamos si funciona:*/
 
-router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
     res.send('PROBAMOS dentro de usuarios')
-})
+}) */
 
+// http://localhost:3000/users/
+
+router.get('/', async (req, res, next) => {
+
+    /* getAll()
+        .then((rows) => {
+
+            res.json(rows)
+        })
+        .catch((err) => { console.log(err); }); */
+
+    /*     try {
+            const rows = await getAll();
+            res.render('clientes/lista', {
+    
+                arrClientes: rows
+    
+            });
+    
+        } catch (err) {
+    
+            console.log(err);
+    
+        } */
+
+
+    try {
+        const getAllUsuarios = await getAll();
+        /* res.json(getAllUsuarios); */
+        console.log(getAllUsuarios);
+    } catch (error) {
+
+        /* res.json({ error: error.message }) */
+        console.log(error);
+
+    }
+
+});
 
 module.exports = router;
